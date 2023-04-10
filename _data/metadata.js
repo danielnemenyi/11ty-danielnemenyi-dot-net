0ab3f43b63dd02bb3fc2497cf39ed272f1df620e
Daniel Nemenyi <daniel@pompo.co>
2023-04-10 17:51:06 +0200

    Basic setup of danielnemenyi.net
    * _data/metadata.js
    * _includes/layouts/base.njk
    * _includes/layouts/home.njk
    * _includes/postslist.njk
    * content/index.njk
    * public/css/index.css

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	"title": "Eleventy Base Blog v8",
-	"url": "https://example.com/",
+	"title": "Daniel Nemenyi",
+	"url": "https://danielnemenyi.net",
 	"language": "en",
-	"description": "I am writing about my experiences as a naval navel-gazer.",
+	"description": "Daniel Nemenyi's homepage",
 	"author": {
-		"name": "Your Name Here",
-		"email": "youremailaddress@example.com",
-		"url": "https://example.com/about-me/"
+		"name": "Daniel Nemenyi",
+		"email": "contact [at] danielnemenyi [dot] net",
+		"url": "https://danielnemenyi.net/about-me/"
 	}
 }
